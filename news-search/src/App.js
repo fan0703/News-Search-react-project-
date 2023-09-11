08b4e2f5912b8ff1af7b06a73e971725c851959f
Fan Yang <yangfanhewangzheng@gmail.com>
2023-09-11 12:01:56 -0400

    searchbar added succesfully

--- news-search/src/App.js
+++ news-search/src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import NewsList from "./components/NewsList";
 import SearchBar from "./components/searchBar/SearchBar";
 function App() {
-  
+  const [articles, setArticles] = useState([]);
+  const getAriticles = async (searchterm) => {
+    const response = await axios.get(
+      `https://newsapi.org/v2/everything?q=${searchterm}&sortBy=publishedAt&apiKey=535a69a0088740faa020385828cb6d70`
+    );
+    console.log(response);
+    setArticles(response.data.articles);
+    //   const data = await response.json()
+    //   setArticles(data)
+  };
+  useEffect(() => {
+    getAriticles("searchterm");
+  }, []);
+
   return (
-    <div className="App">
-      {/* <SearchBar /> */}
-      <NewsList />
+    <div className=" App">
+      <SearchBar articlesearch={getAriticles} />
+      <NewsList articles={articles} />
     </div>
   );
 }
--- news-search/src/components/NewsList.js
+++ news-search/src/components/NewsList.js
@@ -1,28 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import NewsItem from "./NewsItem";
-import SearchBar from "./searchBar/SearchBar";
 
-export default function NewsList() {
-  const [articles, setArticles] = useState([]);
-  const getAriticles = async (searchterm) => {
-    const response = await axios.get(
-      `https://newsapi.org/v2/everything?q=${searchterm}&sortBy=publishedAt&apiKey=535a69a0088740faa020385828cb6d70`
-    );
-    console.log(response);
-    setArticles(response.data.articles);
-    //   const data = await response.json()
-    //   setArticles(data)
-  };
-  useEffect(() => {
-    getAriticles("searchterm");
-  }, []);
+
+export default function NewsList({articles}) {
   return (
     <div className=" NewList">
       {articles.map((article) => {
         return (
           <div className="newList-app">
-            <SearchBar articlesearch={getAriticles} />
+            {/* <SearchBar articlesearch={getAriticles} /> */}
             <NewsItem
               title={article.title}
               description={article.description}
